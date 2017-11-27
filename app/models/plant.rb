e8649829ee696f085eb38a04981f304d90b09c1d
Mathilde Ganancia <mathilde.ganancia@gmail.com>
2017-11-27 14:45:52 +0100

    has_many added to user and destroy dependent changed location to user

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,6 @@
 class Plant < ApplicationRecord
   CATEGORIES = ["plantes vertes", "plantes fleuries", "plantes en pot décoratif", "grandes plantes d'intérieur", "bonsais", "cactus", "plantes artificielles", "arbustes d'extérieurs", "bambous", "fruitiers", "conifères"]
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   validates :name, presence: true
   validates :price_per_day, presence: true
   validates :address, presence: true
