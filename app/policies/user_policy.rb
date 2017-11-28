f94be74f1b92c1e52564b6f0e792fd575eaa98c0
Mathilde Ganancia <mathilde.ganancia@gmail.com>
2017-11-28 16:40:45 +0100

    few changes

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,21 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+   def show?
+    is_user_the_owner_or_admin?
+  end
+
+  def dashboard?
+    is_user_the_owner_or_admin?
+  end
+
+    private
+
+  def is_user_the_owner_or_admin?
+    user == record || user.admin?
+  end
+end
