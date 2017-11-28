b5bc6aa953893108c35fba5acbc54048c25d8681
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-11-28 11:07:06 +0100

    Photos are mandatory to upload when creating a new plant. Sorted array of categories

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,10 +1,11 @@
 class Plant < ApplicationRecord
-  CATEGORIES = ["plantes vertes", "plantes fleuries", "plantes en pot décoratif", "grandes plantes d'intérieur", "bonsais", "cactus", "plantes artificielles", "arbustes d'extérieurs", "bambous", "fruitiers", "conifères"]
+  CATEGORIES = ["Plante Verte", "Plante Fleurie", "Plante en Pot Décoratif", "Grande Plante d'Intérieur", "Bonsaï", "Cactus", "Plante Artificielle", "Arbuste d'Extérieur", "Bambous", "Fruitier", "Conifère"].sort
   belongs_to :user
   has_many :bookings
+  mount_uploader :photo, PhotoUploader
   validates :name, presence: true
   validates :price_per_day, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: CATEGORIES }
-  mount_uploader :photo, PhotoUploader
+  validates :photo, presence: true
 end
