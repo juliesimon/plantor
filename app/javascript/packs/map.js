3c5b9ef6b3d330dc24873468a8447c0a4546d3ec
Alexia Le Tarnec <alexialetarnec@gmail.com>
2017-12-01 15:12:26 +0100

    finished style

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -7,88 +7,100 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const markers = JSON.parse(mapElement.dataset.markers);
   const styles =
      [
-      {
-          "featureType": "all",
-          "elementType": "labels.text",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "all",
-          "elementType": "labels.icon",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "landscape",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#f1efe8"
-              }
-          ]
-      },
-      {
-          "featureType": "landscape.man_made",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "visibility": "on"
-              },
-              {
-                  "gamma": "1.19"
-              }
-          ]
-      },
-      {
-          "featureType": "landscape.man_made",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "visibility": "on"
-              },
-              {
-                  "gamma": "0.00"
-              },
-              {
-                  "weight": "2.07"
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#b2ac83"
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#b2ac83"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#8ac0c4"
-              }
-          ]
-      }
-  ];
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#FFA800"
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#679714"
+            },
+            {
+                "saturation": 33.4
+            },
+            {
+                "lightness": -25.4
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#53FF00"
+            },
+            {
+                "saturation": -73
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#FBFF00"
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#00FFFD"
+            },
+            {
+                "lightness": 30
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#00BFFF"
+            },
+            {
+                "saturation": 6
+            },
+            {
+                "lightness": 8
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    }
+];
   map.addStyle({
     styles: styles,
     mapTypeId: 'map_style'
