eaf3489c25e9636dbfc27ebea59f3cbb78071d3e
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-12-01 15:10:01 +0100

    Changed a lot of style + added stars

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, presence: true
   validates :user, uniqueness: { scope: :booking, message: "you can only leave one review per booking" }
 end
